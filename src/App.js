e19fe37d5fc2d713ec4255b17fd524f7019d2cd4
Akash Pangare <apangare70@gmail.com>
2023-06-14 11:38:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.css";
+import AppHeader from './pages/AppHeader';
+import AppBody from './pages/AppBody';
+import AppFooter from './pages/AppFooter';
 
 // User Defined Tag: <App />
 function App() {
@@ -12,52 +15,11 @@ function App() {
   );
 }
 
-// User Defined Tag: <AppHeader />
-function AppHeader() {
-  return (
-    <div className="row bg-success text-light p-3 fs-1">
-      <div className="col">Home</div>
-      <div className="col">Project</div>
-      <div className="col">AboutUs</div>
-    </div>
-  );
-}
+
 
 // User Defined Tag: <AppBody />
-function AppBody() {
-  return (
-    <>
-      <div className="alert alert-success fs-3 mb-0">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-        rem corrupti ipsam, necessitatibus, ut tempore nisi aspernatur cumque
-        blanditiis quae quia. Corrupti, in ducimus. Ullam suscipit inventore
-        quam ex non. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Voluptatibus tempora eum cupiditate. Nobis id, facere iure debitis eum a
-        at, nulla placeat, tenetur tempore ratione adipisci deleniti alias
-        aspernatur voluptatem! Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Perspiciatis rem corrupti ipsam, necessitatibus, ut
-        tempore nisi aspernatur cumque blanditiis quae quia. Corrupti, in
-        ducimus. Ullam suscipit inventore quam ex non. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Voluptatibus tempora eum cupiditate.
-        Nobis id, facere iure debitis eum a at, nulla placeat, tenetur tempore
-        ratione adipisci deleniti alias aspernatur voluptatem!
-      </div>
-    </>
-  );
-}
 
 // User Defined Tag: <AppFooter />
-function AppFooter() {
-  return (
-    <>
-      <div
-        className="bg-dark text-light fs-1 d-flex justify-content-center align-items-center"
-        style={{ height: "200px" }}
-      >
-        Lorem ipsum dolor sit amet.
-      </div>
-    </>
-  );
-}
+
 
 export default App;
