3a2d6dc75aa633d06ed3cdcaa1a4cdf719fdb051
Akash Pangare <apangare70@gmail.com>
2023-06-14 11:23:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
+// User Defined Tag: <App />
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <AppHeader/>
-      <AppBody/>
-      <AppFooter/>
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
     </>
   );
 }
 
-function AppHeader(){
-  return(
+// User Defined Tag: <AppHeader />
+function AppHeader() {
+  return (
+    <div className="row bg-success text-light p-3 fs-1">
+      <div className="col">Home</div>
+      <div className="col">Project</div>
+      <div className="col">AboutUs</div>
+    </div>
+  );
+}
+
+// User Defined Tag: <AppBody />
+function AppBody() {
+  return (
     <>
-    <h1>AppHeader</h1>
+      <h1>App Body</h1>
     </>
   );
 }
 
-function AppBody(){
-  return(
+// User Defined Tag: <AppFooter />
+function AppFooter() {
+  return (
     <>
-    <h1>App Body</h1></>
+      <h1>App Footer</h1>
+    </>
   );
-}
-
-function AppFooter(){
-  return(
-    <>
-    <h1>App Footer</h1></>
-  )
 }
 
 export default App;
